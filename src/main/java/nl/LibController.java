e6c4b2091bc45d050e39a6b8d859dd1e541fd508
Mark Yuan <myuan@semanticsoftware.com.au>
2017-03-09 18:36:07 +1100

    swagger, bootstrap

--- src/main/java/nl/LibController.java
+++ src/main/java/nl/LibController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class LibController {
 	@Autowired
 	private LibDao libDao;
 	
-	@RequestMapping("/loaddata")
+	@RequestMapping(value="/loaddata", method=RequestMethod.GET)
 	public String loadData() {
 		String clazzPath = LibController.class.getProtectionDomain().getCodeSource().getLocation().getPath();
 		String newPath = clazzPath.substring(0, clazzPath.lastIndexOf("target")) + "data/LibData.csv";
@@ -30,20 +31,20 @@ public class LibController {
 		return "DONE";
 	}
     
-    @RequestMapping("/")
-    public Map<String,Object> index() {
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public @ResponseBody Map<String,Object> index() {
 		Map<String, Object> model = new HashMap<String, Object>();
 		model.put("date", new Date().toString());
 		model.put("content", "Greetings from MY");
 		return model;
     }
     
-    @RequestMapping("/books")
+    @RequestMapping(value="/books", method=RequestMethod.GET)
     public @ResponseBody List<Book> books() {
     	return libDao.findAllBook();
     }
     
-    @RequestMapping("/addperson")
+    @RequestMapping(value="/addperson", method=RequestMethod.GET)
     public @ResponseBody long addPerson(@RequestParam(name="name") String name,
     		@RequestParam(name="phoneNumber") String phoneNumber, @RequestParam(name="emailAddress") String emailAddress) {
     	Person person = new Person();
@@ -53,17 +54,17 @@ public class LibController {
     	return libDao.addPerson(person);
     }
     
-    @RequestMapping("/persons")
+    @RequestMapping(value="/persons", method=RequestMethod.GET)
     public @ResponseBody List<Person> persons() {
     	return libDao.findAllPerson();
     }
     
-    @RequestMapping("/lend/{personId}/{bookId}")
+    @RequestMapping(value="/lend/{personId}/{bookId}", method=RequestMethod.POST)
     public @ResponseBody Boolean lend(@PathVariable long personId, @PathVariable long bookId) {
     	return libDao.lend(personId, bookId);
     }
     
-    @RequestMapping("/booklent/{personId}")
+    @RequestMapping(value="/booklent/{personId}", method=RequestMethod.GET)
     public @ResponseBody List<Book> booklent(@PathVariable long personId) {
     	return libDao.findBookByPerson(personId);
     }
